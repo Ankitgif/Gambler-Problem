7589c3d7dbc750ed723711b3f56edc1bd35452be
Ankitgif <ankitsinha2303@gmail.com>
2020-05-22 20:36:26 +0530

    [Ankit] Added code to view number of time win or loss

---
+++ Gambler.js
@@ -0,0 +1,31 @@
+const input = require('readline-sync');
+let noOfTrial = input.questionInt("Enter number of trial :");
+let stake = input.questionInt("Enter stake in INR :");
+let goal = input.questionInt("Enter goal in INR :");
+let bet = 0, win = 0, loss = 0;
+for(let trial=0;trial<noOfTrial;trial++)
+{
+    let cash = stake;
+    while(cash>0 && cash<goal)
+    {
+        bet++;
+        if(Math.random() < 0.5)
+        {
+            cash++;
+        }
+        else
+        {
+            cash--;
+        }
+    }
+    if(cash == goal)
+    {
+        win++;
+    }
+    else
+    {
+        loss--;
+    }
+}
+console.log("Number of time win :"+win);
+console.log("Number of time loss :"+loss);
